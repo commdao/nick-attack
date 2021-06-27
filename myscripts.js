0a8756de6c21148f1db1abc093a066ee04f4234c
Jon Dao <johnnys.second.opinion@gmail.com>
2021-06-27 15:04:01 -0400

    magic expansion

--- myscripts.js
+++ myscripts.js
@@ -12,6 +12,12 @@
    var enemyHealthField = document.querySelector(".enemy-health-field");
    var enemyHealth = 100;
 
+   // get player MP
+   var magicPoints = document.querySelector(".player_magic")
+   var baseMagic = 50;
+   // Seems like this will be a bit more complicated than subtracting enemy health
+   // Yes, we will deduce from our starting total, but we also have to create values for each magic attack
+
    attackButton.addEventListener("click", attack)
    magicButton.addEventListener("click", magicAttack)
    itemButton.addEventListener("click", listItems)
@@ -24,6 +30,8 @@
    var crticalChoice = new Set([5, 7, 9])
 
    var recentAttacks = []
+   // adding a magic plural to match the line above, but don't understand this part, I see it's referenced once
+   var recentMagicAttacks = []
 
    var items = ['potion','antidote','phoenix down']
    
@@ -59,16 +67,40 @@
        return recentAttack
    }
 
+   // matching the magicAttack function to be in line with the LINE 30 function
+   // having issue with enemy HP jumping up / testing and sometimes physical attacks adding health too
+   // watching the console log, seems to be something with the multiplier math
    function magicAttack() {
         if (enemyHealth <= 0){
             return damageField.innerText = "enemy is already dead"
         }
-       var recentMagicAttack = getRandoNumber();
-            console.log(recentMagicAttack)
-        if (recentMagicAttack == 3){
-            recentMagicAttack += 21
-        }
-       damageType.innerText = 'magical';
-
-       damageField.innerText = recentMagicAttack
+        var recentMagicAttack = getRandoNumber();
+        recentMagicAttack = magicMultiplier(recentMagicAttack)
+        var newEnemyHp = (enemyHealth - recentMagicAttack);
+        enemyHealthField.innerText = newEnemyHp
+        damageField.innertext = recentMagicAttack
+        recentMagicAttacks.push(recentMagicAttack)
+        damageType.innerText = 'magical';
+        // trying to set Magic Attack Color
+        damageType.style.color = "red";
+        damageType.style.backgroundColor = "black";
+        return enemyHealth
    }
+
+      // creating a separate multiplier for magic, want a lower chance but higher bonus
+      function magicMultiplier(recentMagicAttack) {
+        if (recentMagicAttack == 4){
+            recentMagicAttack *= 5
+        } else {
+            console.log('magic bonus failed')
+        }
+        return recentMagicAttack
+    }
+
+    // creating simple function for MP cost ... where do I insert this?
+    function magicDeduction() {
+        var castSpell = 5;
+        var newMp = (baseMagic - castSpell)
+        player_magic.innerText = newMp
+        return newMp
+    }
