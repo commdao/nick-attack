5186604b081895f034b1af3bf358d8e53ecf121c
30ozsteak <ndambro303@gmail.com>
2021-07-11 13:47:10 -0600

    updates

--- myscripts.js
+++ myscripts.js
@@ -1,49 +1,133 @@
-// Get the things from the HTML / REF content
-   let restartButton = document.querySelector('.restart-button');
-   let attackButton = document.querySelector(".attack-button");
+// routes
+   let startScreen = document.querySelector('.start_screen');
+   let homeScreen = document.querySelector('.home_screen');
+   let leaderboardScreen = document.querySelector('.leaderboard_screen');
+   let battleScreen = document.querySelector('.battle_screen');
+   let settingsScreen = document.querySelector('.settings_screen');
 
-   let enemyHealthField = document.querySelector(".enemy-health-field");
-   let enemyHealth = 100;
-   let enemyHealthBar= document.getElementById("health")
-   let damageCounter = []
-   let damageOutput = document.querySelector('.damage-output');
+// buttons
+   let homeButton = document.querySelector('.home_button')
+   let refreshLeaderboardButton = document.querySelector('.refresh_lb_button')
+   let exitButton = document.querySelectorAll('.exit_button')
+   let leaderboardButton = document.querySelectorAll('.leaderboard_button')
+   let settingsButton = document.querySelectorAll('.settings_button');
+   let saveButton = document.querySelector('.save_button')
 
-   let gameStarted = false;
-   let playerHealthField = document.querySelector('.player-health-field')
-   let playerHealth = 100;
+// event listeners - routes
+   homeButton.addEventListener('click', homeRoute);
+   refreshLeaderboardButton.addEventListener('click', refreshLeaderboard);
+   settingsButton.forEach(btn => btn.addEventListener('click', settingsRoute));
+   leaderboardButton.forEach(btn => btn.addEventListener('click', leaderboardRoute));
+   exitButton.forEach(btn => btn.addEventListener('click', startRoute));
 
-   attackButton.addEventListener("click", attack)
-   restartButton.addEventListener('click', restart)
+   // event listeners - menu actions
+   saveButton.addEventListener('click', saveGame);
 
-   function restart()  {
-    location.reload();
-   }
-
-   function startGame(){
-       gameStarted = true;
-   }
-
-   function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-   }
-   
-   function attack(target){
-       let finalDamage = getRandomNumber(player.level, player.atk)
-       damageCounter.push(finalDamage);
-       displayDamage(finalDamage)
-    //    dealDamage(target, finalDamage)
-       return finalDamage
-   }
-
-   function displayDamage(num){
-    damageOutput.innerHTML =`<li>you did ${num} damage</li>` 
-   }
+// dynamic content
+   let playerDisplay = document.querySelector('.player-display');
+   let leaderboardDisplay = document.querySelector('.leaderboard_display');
 
     var player = {
-        name: '',
-        level: 1,
-        exp: 0,
-        atk: 5,
-        pwr: 5,
-        abilities: [],
+    name: 'Steaks McGee',
+    level: 1,
+    exp: 0,
+    abilities: [],
     }
+
+    function startRoute(){
+    console.log('returned to start menu');
+    battleScreen.style.display = "none";
+    settingsScreen.style.display ="none";
+    leaderboardScreen.style.display = "none";
+    startScreen.style.display = "block";
+    homeScreen.style.display = "none";
+    }
+
+   function homeRoute(){
+    console.log('redirected to home');
+    battleScreen.style.display = "none";
+    settingsScreen.style.display ="none";
+    leaderboardScreen.style.display = "none";
+    startScreen.style.display = "none";
+    homeScreen.style.display = "block";
+    displayPlayerStats();
+   }
+
+   function leaderboardRoute(){
+    console.log('redirected to leaderboard');
+    settingsScreen.style.display ="none";
+    battleScreen.style.display ="none";
+    homeScreen.style.display = "none";
+    startScreen.style.display = "none";
+    leaderboardScreen.style.display = "block";
+    console.log('leaderboard updating...');
+    setTimeout(() => {
+            console.log('leaderboard updated');
+            displayLeaderboard();
+        }, 1500)
+   };
+
+
+    function settingsRoute(){
+    console.log('redirected to settings');
+    battleScreen.style.display ="none";
+    homeScreen.style.display = "none";
+    startScreen.style.display = "none";
+    leaderboardScreen.style.display = "none";
+    settingsScreen.style.display ="block";
+    };
+
+
+//    Leaderboard 
+
+   function refreshLeaderboard(){
+    console.log('leaderboard updating...');
+    setTimeout(() => {
+        displayLeaderboard();
+    }, 1500)
+    console.log('leaderboard updated');
+   }
+
+   function displayLeaderboard(){
+
+   }
+
+//    Battle
+
+   function displayPlayerStats(){
+    playerDisplay.innerHTML = `<li>${player.name}</li><br/><li>${player.level}</li><br/><li>EXP: ${player.exp} </li>`
+   }
+
+//    settings
+
+function createPlayer(e){
+    e.preventDefault();
+    let playerName = playerName.value;
+    let mostRecentPlayer = new PlayerGenerator(playerName);
+    prependPlayer(mostRecentPlayer);
+    saveGame(mostRecentIdea);
+}
+
+function PlayerGenerator(player){
+  this.name = player;
+  this.exp = 0;
+  this.id = Date.now();
+}
+
+// 
+  function saveGame(player) {
+    var save = JSON.stringify(player);
+    localStorage.setItem(player.id, save);
+    console.log('save complete. your challenge code is',  player.id)
+  };
+
+  function loadSaves() {
+    for (var i = 0; localStorage.length; i++) {
+      var code = JSON.parse(localStorage.getItem(localStorage.key(i)));
+      prependIdea(code);
+    }
+  };
+
+
+//    player
+
