838f7516edb31c3d75057714898dbf15e2a6b7c5
CommDao Laptop <commdao@gmail.com>
2021-08-13 14:50:20 -0400

    15 characters created

---
+++ scripts/ikea.js
@@ -0,0 +1,170 @@
+// simple RPS = Mage > Knight > Summoner > Mage 
+// exception is the Feeder, think Moogles/Cactuars in FFBE
+// rarity level = skills alloted, e.g. 2* rarity only has 2 skills
+
+const characterData = {
+    characters: [
+      {
+        name: 'Godtagbar',
+        img: URL(''),
+        rarity: '2'
+        type: 'vase',
+        roles: ['magic'],
+        skills: [
+          '#', 
+          '#',
+        ],
+        name: 'Ribba',
+        img: URL(''),
+        rarity: '3'
+        type: 'frame',
+        roles: ['summoner'],
+        skills: [
+          '#', 
+          '#', 
+          '#', 
+        ],
+        name: 'Hauga',
+        img: URL(''),
+        rarity: '4'
+        type: 'tv unit',
+        roles: ['knight'],
+        skills: [
+          '#', 
+          '#', 
+          '#',
+          '#', 
+        ],
+        name: 'Ekedalen',
+        img: URL(''),
+        rarity: '3'
+        type: 'bar stool',
+        roles: ['mage'],
+        skills: [
+          '#', 
+          '#', 
+          '#', 
+        ],
+        name: 'Janinge',
+        img: URL(''),
+        rarity: '3'
+        type: 'bar stool',
+        roles: ['mage'],
+        skills: [
+          '#', 
+          '#', 
+          '#', 
+        ],
+        name: 'Nordviken',
+        img: URL(''),
+        rarity: '3'
+        type: 'bar stool',
+        roles: ['mage'],
+        skills: [
+          '#', 
+          '#', 
+          '#', 
+        ],
+        name: 'Hansolle',
+        img: URL(''),
+        rarity: '3'
+        type: 'bar stool',
+        roles: ['summoner'],
+        skills: [
+          '#', 
+          '#', 
+          '#', 
+        ],
+        name: 'Jokkmokk',
+        img: URL(''),
+        rarity: '5'
+        type: [ 
+            'bar stool',
+            'table'
+        ],
+        roles: ['mage', 'knight'],
+        skills: [
+          '#', 
+          '#', 
+          '#',
+          '#',
+          '#', 
+        ],
+        name: 'Fixa',
+        img: URL(''),
+        rarity: '1'
+        type: 'stickers',
+        roles: ['feeder'],
+        skills: [
+          '#', 
+        ],
+        name: 'Volfgang',
+        img: URL(''),
+        rarity: '3'
+        type: 'bar stool',
+        roles: ['summoner'],
+        skills: [
+          '#', 
+          '#', 
+          '#', 
+        ],
+        name: 'Brimnes',
+        img: URL(''),
+        rarity: '4'
+        type: 'wardrobe',
+        roles: ['knight'],
+        skills: [
+          '#', 
+          '#', 
+          '#',
+          '#', 
+        ],
+        name: 'Trysel',
+        img: URL(''),
+        rarity: '5'
+        type: 'wardrobe',
+        roles: ['summoner', 'knight'],
+        skills: [
+          '#', 
+          '#', 
+          '#',
+          '#',
+          '#', 
+        ],
+        name: 'Songesand',
+        img: URL(''),
+        rarity: '4'
+        type: 'wardrobe',
+        roles: ['knight'],
+        skills: [
+          '#', 
+          '#', 
+          '#',
+          '#', 
+        ],
+        name: 'Ingatorp',
+        img: URL(''),
+        rarity: '4'
+        type: 'table',
+        roles: ['summoner'],
+        skills: [
+          '#', 
+          '#', 
+          '#',
+          '#', 
+        ],
+        name: 'Kornsjo',
+        img: URL(''),
+        rarity: '5'
+        type: [
+            'cabinet', 
+            'mirror'
+        ],
+        roles: ['summoner'],
+        skills: [
+          '#', 
+          '#', 
+          '#', 
+        ],
+    }
+]}
